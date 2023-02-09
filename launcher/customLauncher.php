10a551d5e30497b6aafc3e6b1dcf2050a638ed8c
Juan <rigualjuan@gmail.com>
2023-02-09 10:25:08 -0300

    intercept uri and parse query params to redirect to commonLauncher from customLauncher

--- launcher/customLauncher.php
+++ launcher/customLauncher.php
@@ -1,3 +1,13 @@
 <?php
 // Here we expected parse the launcher url and redirect to commonLauncher.php
 // Code ...
+
+$id = $_GET['userid'] ? $_GET['userid'] : "";
+$operator = $_GET['establishment'] ? $_GET['establishment'] : "";
+$language = $_GET['locale'] ? $_GET['locale'] : "";
+$currency = $_GET['coin'] ? $_GET['coin'] : "";
+$token = $_GET['securecode'] ? $_GET['securecode'] : "";
+
+ header("Location: ./commonLauncher.php?id=$id&operator=$operator&language=$language&currency=$currency&token=$token");
+
+echo $id, $operator, $language, $currency, $token;
